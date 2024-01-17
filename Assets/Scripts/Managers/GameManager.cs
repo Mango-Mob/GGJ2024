1ca1af2cabcf136ac6a2fea5bd1e5cd3be483ad5
Michael Jordan <michdan25@gmail.com>
2024-01-17 23:05:13 +1300

    initial comit

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public GameObject m_player;
+    public Camera m_activeCamera;
+    public bool IsInCombat = false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_player = GameObject.FindGameObjectWithTag("Player");
+        m_activeCamera = Camera.main;
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        m_player = GameObject.FindGameObjectWithTag("Player");
+        m_activeCamera = Camera.main;
+    }
+}
